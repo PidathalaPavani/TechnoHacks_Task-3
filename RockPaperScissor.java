0d8bcf72d0a343fcbcdf54dc7ce9b48938ca9965
PidathalaPavani <pidathalapavani25@gmail.com>
2024-05-21 13:47:23 +0530

    Add files via upload

---
+++ RockPaperScissor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class RockPaperScissor{
+    public static void main(String[] args){
+         Scanner input = new Scanner(System.in);
+
+            int computer = (int)(Math.random() * 3);
+                System.out.print("scissor (0), rock(1), paper(2): ");
+                      int user = input.nextInt();
+                System.out.print("The computer is ");
+                       switch(computer){
+           case 0: System.out.print("scissor.");
+                  break;
+           case 1: System.out.print("rock.");
+                  break;
+           case 2: System.out.print("paper");
+            }
+              if(computer == user)
+                 System.out.println(" too. It is a draw");
+                 else
+                 {
+                   Boolean win = (user == 0 && computer == 2) ||
+                                 (user == 1 && computer ==0) ||
+                                 (user == 2 && computer ==1);    
+                 if (win)
+                    System.out.println(".You won");
+                 else
+                    System.out.println(".You lose");  
+                  }
+           }
+ }
